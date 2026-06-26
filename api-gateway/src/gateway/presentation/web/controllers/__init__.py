5fa5b5bcc3b3c039877726dfa5a04b4fce57e477
batmaxcom <batmanovv@kamaz.ru>
2026-06-26 09:10:45 +0300

    fix(gateway): Упрозднил сервис gateway

--- api-gateway/src/gateway/presentation/web/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .healthcheck import HEALTHCHECK_CONTROLLER
-from .proxy import PROXY_CONTROLLER, ROUTES, setup_proxy_routes
-
-__all__ = (
-    "HEALTHCHECK_CONTROLLER",
-    "PROXY_CONTROLLER",
-    "ROUTES",
-    "setup_proxy_routes",
-)
